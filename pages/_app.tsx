9920864cae4f57984a7adf21fbb4be6ccf97a5c3
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-09-12 01:16:30 +0900

    feat: meta tags integrations on document private file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-// @ts-ignore
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { light } from "../scss/MaterialTheme";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState(createTheme());
   return <Component {...pageProps} />;
 }
