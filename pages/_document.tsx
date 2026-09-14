6189781ace07b6d4d4cefa537b9d4174fa0824ab
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-09-14 20:10:22 +0900

    feat: modifier files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,16 +9,16 @@ export default function Document() {
         <meta name="robots" content="index,follow" />
         <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
 
-        {/* SEO Search Engine Optimization (veb-saytni qidiruv tizimlarida) */}
+        {/* SEO */}
         <meta
           name="keyword"
-          content={"nestar, nestar.uz, devex mern,mern nestjs fullstack"}
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
         />
         <meta
           name="description"
           content={
             "Buy and sell properties anywhere anytime in South Korea. | " +
-            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. |" +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. | " +
             "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
           }
         />
